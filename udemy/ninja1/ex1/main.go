1f1393ea3494b18bf8b09c4b9b40039ca2c81d8d
Kunmeer-SyedMohamedHyder <syed-mohamed-hyder.kunmeer@hpe.com>
2022-11-04 14:22:17 +0530

    Completed exercise1 of ninja1

---
+++ udemy/ninja1/ex1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Printf("type(x)=%T\n", x)
+	fmt.Printf("type(y)=%T\n", y)
+	fmt.Printf("type(z)=%T\n", z)
+}
