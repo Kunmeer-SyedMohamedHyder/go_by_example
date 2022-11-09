16368fefe3ad2c1d38db4aa598f4d36c61ca3299
Kunmeer-SyedMohamedHyder <syed-mohamed-hyder.kunmeer@hpe.com>
2022-11-09 10:40:00 +0530

    Understood type interfaces

---
+++ udemy/ninja9/ex2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func (p *person) speak() string {
+	return fmt.Sprintf("I am %v and I am %v years old", p.name, p.age)
+}
+
+func (p *person) speak2() string {
+	return fmt.Sprintf("I am %v and I am %v years old", p.name, p.age)
+}
+
+type human interface {
+	speak() string
+}
+
+func say_something(h human) string {
+	return h.(*person).speak2()
+}
+
+func main() {
+	p := person{
+		name: "Syed",
+		age:  22,
+	}
+	s := p.speak()
+	fmt.Println(s)
+	s = say_something(&p)
+	fmt.Println(s)
+
+}
